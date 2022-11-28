a8de82da0f9a00b90ab64ad08d60d74dc0f40457
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2022-11-28 02:42:38 -0700

    Fix excess folders/files

--- config.ts
+++
@@ -1,3 +0,0 @@
-export const infuraKey = process.env.REACT_APP_INFURA_KEY;
-export const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-export const openSeaUrl = process.env.REACT_APP_OPENSEA_URL || "";
--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    env: any; // 👈️ turn off type checking
-  }
-}
